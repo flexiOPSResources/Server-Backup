cdb720ba904abb5c331b6348ba573f37a43b5a0b
flexiOPS <aphee888@gmail.com>
2016-09-12 13:05:10 +0100

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -2,6 +2,6 @@ export PASSPHRASE=yourPassPhrase
 export FTP_PASSWORD=backupServerPassword
 
 #--no-encryption is used only if the backed up files were encrypted using a passphrase
-duplicity --no-encryption ftp://username@domain/backupDirectory /home/username/backupDestination
+duplicity --no-encryption ftp://username@domain/serverBackupDirectory /home/username/backupDestination
 unset PASSPHRASE
 unset FTP_PASSWORD
